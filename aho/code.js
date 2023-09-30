8a1326e5c34612f98fd78ae751df7d760bed250f
tabito-web <146086315+tabito-web@users.noreply.github.com>
2023-09-30 17:59:58 +0900

    Add files via upload

---
+++ aho/code.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", function () {
+    const foot = document.querySelector("#foot");
+    const scroll = window.pageYOffset;
+    if (scroll > 700) {
+      foot.style.opacity = "1";
+      // console.log(scroll);
+    } else {
+      foot.style.opacity = "0";
+      // console.log(scroll);
+    }
+  });
+  window.addEventListener("scroll", function () {
+    const foot = document.querySelector("#foot");
+    const scroll = window.pageYOffset;
+    if (scroll == 0) {
+      foot.style.opacity = "0";
+      // console.log(scroll);
+    }
+  });
